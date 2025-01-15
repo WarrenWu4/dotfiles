b1d7cef66aa65a2527f827d79fe7ce5199793c12
WarrenWu4 <warrenweiwu04@gmail.com>
2025-01-15 11:56:44 -0600

    quick fix :)

--- nvim/lua/warrenwu/remap.lua
+++ nvim/lua/warrenwu/remap.lua
@@ -9,5 +9,5 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- telescope remaps
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.find_files, { desc = 'Telescope git files' })
+vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope git files' })
 
