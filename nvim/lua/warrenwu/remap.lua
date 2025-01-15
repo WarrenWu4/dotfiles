be407689f01779b98922e0cdb5c2cdea9a1e67a4
WarrenWu4 <warrenweiwu04@gmail.com>
2025-01-15 11:53:22 -0600

    added git only remap for telescope

--- nvim/lua/warrenwu/remap.lua
+++ nvim/lua/warrenwu/remap.lua
@@ -8,5 +8,6 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- telescope remaps
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.find_files, { desc = 'Telescope git files' })
 
