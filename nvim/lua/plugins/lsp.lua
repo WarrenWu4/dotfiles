e1e07ae058dced4747369bcc467c09fe1974770b
WarrenWu4 <warrenweiwu04@gmail.com>
2025-02-26 15:47:06 -0600

    added diagnostics

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -87,7 +87,7 @@ return {
                 desc = 'LSP actions',
                 callback = function(event)
                     local opts = {buffer = event.buf}
-
+                    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
                     vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
                     vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
                     vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
