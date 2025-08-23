a1c5971ed6a9a5fc768e5ea0c82528a9b7e763b3
WarrenWu4 <warrenweiwu04@gmail.com>
2025-08-23 01:46:57 -0500

    added todo nvim packages + remapped lsp formatter

--- nvim/lua/plugins/comments.lua
+++ nvim/lua/plugins/comments.lua
@@ -1,3 +1,33 @@
 return {
-    -- 
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            keywords = {
+                INFO = {icon = "∩ä⌐", color = "info", alt = { "info", "Info" } },
+                TODO = { icon = "∩ÆÉ ", color = "warning" },
+                FIX = { icon = "∩åê ", color = "error", alt = { "FIXME", "BUG", "ISSUE" } },
+            },
+            highlight = {
+                before = "",
+                keyword = "fg",
+                after = "fg",
+            }
+        },
+    },
+    {
+        "echasnovski/mini.comment",
+        version = false, -- always use latest
+        config = function()
+            require("mini.comment").setup({
+                -- Optional: customize mappings
+                mappings = {
+                    comment = "gc",  -- toggle comment for motion/textobject
+                    comment_line = "gcc", -- toggle comment for line
+                    textobject = "gc", -- comment textobject (works with motions like `gcip`)
+                },
+            })
+        end,
+    }
 }
+
